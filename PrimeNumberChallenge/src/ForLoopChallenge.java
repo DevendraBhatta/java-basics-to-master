ff1fa2ef4dcdb0c037d70d35f01d7eb36104d76f
DevendraBhatta <devendrabhatta63@gmail.com>
2025-07-10 09:48:59 +0545

    update challenge for loop

--- PrimeNumberChallenge/src/ForLoopChallenge.java
+++ PrimeNumberChallenge/src/ForLoopChallenge.java
@@ -1,15 +1,39 @@
+import java.util.Arrays;
+
 public class ForLoopChallenge {
+
     public static void main(String[] args) {
 
+        int count = 0;
+        int[] primeNumbers = new int[11];
+        int primeIndex = 0;
 
-        for (int i = 0; i <= 1000; i++) {
-            System.out.println(i + " is " + (isPrime(i) ? " " : " Not ") + " a prime Number");
+        for (int i = 10; i <= 50; i++) {
+            if (isPrime(i)) {
+                primeNumbers[primeIndex] = i;
+                primeIndex++;
+                count++;
+            }
         }
+        System.out.println("There are " + count + "Prime Numbers");
+        System.out.println("They are " + Arrays.toString(primeNumbers));
+
     }
+
 
     public static boolean isPrime(int wholeNumber) {
 
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+        if (wholeNumber % 2 == 0) {
+            return false;
+        }
+        for (int divisor = 3; divisor < (wholeNumber / 2); divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
         return true;
-
     }
 }
--- PrimeNumberChallenge/src/Main.java
+++ PrimeNumberChallenge/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " is " + (isPrime(i) ? " SURE" : "NOT") + " a Prime Number");
+            System.out.println(i + " is " + (isPrime(i) ? "" : "NOT") + " a Prime Number");
         }
     }
 
@@ -9,8 +9,11 @@ public class Main {
         if (wholeNumber <= 2) {
             return (wholeNumber == 2);
         }
+        if (wholeNumber % 2 == 0){
+            return false; // even number are out
+        }
 
-        for (int divisor = 2; divisor <= wholeNumber/2; divisor++) {
+        for (int divisor = 3; divisor <= Math.sqrt(wholeNumber); divisor++) {
             if (wholeNumber % divisor == 0) {
                 return false;
             }
