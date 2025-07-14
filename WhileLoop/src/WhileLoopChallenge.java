ab01d962087a0abe6fd30caa46d359e9c3f5a00a
DevendraBhatta <devendrabhatta63@gmail.com>
2025-07-14 13:49:13 +0545

    comitting...

--- WhileLoop/src/WhileLoopChallenge.java
+++ WhileLoop/src/WhileLoopChallenge.java
@@ -2,18 +2,32 @@ public class WhileLoopChallenge {
     public static void main(String[] args) {
 
         int num = 5;
+        int finishNumber = 20;
 
-        while (num <= 20) {
-            System.out.println(num +" is "+(isEvenNumber(num)?" even ":" odd")+" number");
+        while (num <= finishNumber) {
+
+            // This code will remove not even Number (odd Number)
+//            if (!isEvenNumber(num)){
+//                continue;
+//            }
+
+            System.out.println(num + " is " + (isEvenNumber(num) ? " even " : " odd") + " number");
             num++;
 
         }
     }
 
     public static Boolean isEvenNumber(int number) {
-        while (number % 2 == 0) {
+        // this will also work but if is recommended
+//        while (number % 2 == 0) {
+//            return true;
+//        }
+//        return false;
+
+        if ((number % 2 == 0)) {
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 }
