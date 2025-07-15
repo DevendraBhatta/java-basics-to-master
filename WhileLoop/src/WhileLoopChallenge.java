6e62e757db9ec42bba1f4eb29adfe027cf35fb64
DevendraBhatta <devendrabhatta63@gmail.com>
2025-07-15 14:59:07 +0545

    Count even and odd numbers in while loop
    Refactored the while loop to count and print the number of even and odd numbers found between 4 and 20. The loop now stops after finding 5 even numbers and prints the counts for both even and odd numbers.

--- WhileLoop/src/WhileLoopChallenge.java
+++ WhileLoop/src/WhileLoopChallenge.java
@@ -1,15 +1,25 @@
 public class WhileLoopChallenge {
     public static void main(String[] args) {
 
-        int num = 5;
+        int num = 4;
+        int evenCount = 0;
+        int oddCount = 0;
         int finishNumber = 20;
 
         while (num <= finishNumber) {
-
-            System.out.println(num + " is " + (isEvenNumber(num) ? " even " : " odd") + " number");
             num++;
-
+            if (!isEvenNumber(num)){
+                oddCount++;
+                continue;
+            }
+            evenCount ++;
+            System.out.println("Even Number " + num);
+            if (evenCount >= 5){
+                break;
+            }
         }
+        System.out.println(evenCount+ " even numbers are found");
+        System.out.println(oddCount+ " odd numbers are found");
     }
 
     public static Boolean isEvenNumber(int number) {
