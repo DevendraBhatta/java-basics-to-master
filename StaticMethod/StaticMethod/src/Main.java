69592c9e1b7b296643c5fb033eefa45a0250b78a
DevendraBhatta <devendrabhatta63@gmail.com>
2025-07-21 11:49:09 +0545

    update

---
+++ StaticMethod/StaticMethod/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator.sum(5, 10);
+        printHello();
+    }
+
+
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+}
